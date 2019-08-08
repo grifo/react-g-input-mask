446b08c21cfe5c1f6798f7511740f9982d11ffa1
Renatho <renatho@gmail.com>
2019-08-08 14:58:23 -0300

    Add general configs (tests, babel, lint, storybook, etc.)

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  setupFilesAfterEnv: [
+    '@testing-library/react/cleanup-after-each',
+    './setupTests.js'
+  ]
+}
